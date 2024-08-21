190dec467e3e3d8fed3b048a0c1167e7f1f44168
Olooce <asirastephen9@gmail.com>
2024-08-21 23:27:38 +0300

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 echo "Building Load Balancer..."
-cd ../cmd/load-balancer
+cd ./cmd/load-balancer || { echo "Directory not found"; exit 1; }
 go build -o load-balancer
 echo "Build complete."
